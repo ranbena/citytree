2013ed1ad53940f5ad08f2f67ff29b6dfe1b2ee6
Ran Byron <ranbena@gmail.com>
2020-06-12 19:57:41 +0300

    Added social buttons

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,6 +3,9 @@ import { Container } from 'react-bootstrap';
 import { MdLink } from 'react-icons/md';
 import logo from '../../images/logo.png';
 import { phone, email } from '../../constants';
+import FacebookIcon from '../../images/social_facebook.svg';
+import InstagramIcon from '../../images/social_instagram.svg';
+import YoutubeIcon from '../../images/social_youtube.svg';
 import './styles.scss';
 
 export const Footer: React.FC<{}> = () => (
@@ -12,11 +15,22 @@ export const Footer: React.FC<{}> = () => (
         <a href="http://archives.citytree.net/" target="_blank" rel="noopener noreferrer">
           <MdLink /> אל האתר הישן והאהוב
         </a>
-        <img src={logo} alt="CityTree" height="100" />
+        <img src={logo} alt="CityTree" className="logo" />
         <div id="info">
           אנחנו כאן
           <br />
           <a href={`tel:${phone}`}>{phone}</a> | <a href={`mailto:${email}`}>{email}</a>
+          <div className="social">
+            <a href="https://www.youtube.com/user/citytreenet/">
+              <img src={YoutubeIcon} alt="עץבעיר ביוטיוב" />
+            </a>
+            <a href="https://www.instagram.com/citytreetlv/">
+              <img src={InstagramIcon} alt="עץבעיר באינסטגרם" />
+            </a>
+            <a href="https://facebook.com/citytree">
+              <img src={FacebookIcon} alt="עץבעיר בפייסבוק" />
+            </a>
+          </div>
         </div>
       </div>
     </Container>
