dac67b75e296f59a2dca4758c86a31901445e476
Ran Byron <ranbena@gmail.com>
2020-12-12 15:05:38 +0200

    Updates

---
+++ component---src-pages-404-tsx-9b67066942fd7aee431c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),o=t.n(a);e.default=function(){return o.a.createElement("main",null,o.a.createElement("p",null,"Sorry, page not found!"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-9b67066942fd7aee431c.js.map
