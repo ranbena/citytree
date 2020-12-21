337504df085d8787a8a19512f00cecdfa54df030
Ran Byron <ranbena@gmail.com>
2020-12-21 19:59:11 +0200

    Updates

--- component---src-pages-404-tsx-4ba03e850ee49e3ace84.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"i6+/":function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),o=t.n(a);e.default=function(){return o.a.createElement("main",null,o.a.createElement("p",null,"Sorry, page not found!"))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-4ba03e850ee49e3ace84.js.map
