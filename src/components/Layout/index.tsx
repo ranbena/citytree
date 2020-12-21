d1fe5b31647178189f094f0b0f5c53ab4ec09633
Ran Byron <ranbena@gmail.com>
2020-12-21 20:19:58 +0200

    i18n meta data

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,38 +1,49 @@
 import React, { FC } from 'react';
 import { Helmet } from 'react-helmet';
 import { useIntl } from 'gatsby-plugin-intl';
-import { meta } from '@/constants';
+
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
+import ogImage from '@/images/og.png';
+import favicon from '@/images/favicon.ico';
+
 import './styles.scss';
 
-const pathPrefix = process.env.GATSBY_PATH_PREFIX || '';
-const cannonicalUrl = `${meta.siteUrl}${pathPrefix}`;
+const siteUrl = process.env.GATSBY_SITE_URL;
+const image = {
+  path: ogImage,
+  width: '765',
+  height: '765',
+};
 
 const Layout: FC = ({ children }) => {
   const intl = useIntl();
   const dir = intl.formatMessage({ id: 'direction' });
+  const title = intl.formatMessage({ id: 'meta.title' });
+  const keywords = intl.formatMessage({ id: 'meta.keywords' });
+  const description = intl.formatMessage({ id: 'meta.description' });
+  const author = intl.formatMessage({ id: 'meta.author' });
+  const siteName = intl.formatMessage({ id: 'citytree' });
+  const locale = intl.formatMessage({ id: 'meta.locale' });
 
   return (
     <>
       <Helmet bodyAttributes={{ dir }}>
         <meta charSet="utf-8" />
-        <title>{meta.title}</title>
-        <link rel="canonical" href={cannonicalUrl} />
-        <meta name="keywords" content={meta.keywords} />
-        <meta name="description" content={meta.description} />
-        <meta name="author" content={meta.author} />
-        <link rel="shortcut icon" href={meta.favicon} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:locale" content="he_IL" />
+        <title>{title}</title>
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+        <meta name="author" content={author} />
+        <link rel="shortcut icon" href={favicon} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:locale" content={locale} />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content={cannonicalUrl} />
-        <meta property="og:site_name" content={meta.siteName} />
-        <meta property="og:image" content={`${meta.siteUrl}${meta.image.path}`} />
-        <meta property="og:image:width" content={meta.image.width} />
-        <meta property="og:image:height" content={meta.image.height} />
+        <meta property="og:site_name" content={siteName} />
+        <meta property="og:image" content={`${siteUrl}${image.path}`} />
+        <meta property="og:image:width" content={image.width} />
+        <meta property="og:image:height" content={image.height} />
       </Helmet>
       <Header />
       {children}
--- src/constants.tsx
+++ src/constants.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import ogImage from './images/og.png';
-import favicon from './images/favicon.ico';
-
 export const phone = '03-5254196';
 export const bitPhone = '058-4227531';
 export const email = 'tree@citytree.net';
@@ -11,19 +8,3 @@ export const address = 'ביאליק 25 דירה 7';
 export const phoneLink = <a href={`tel:${phone}`}>{phone}</a>;
 export const emailLink = <a href={`mail:${email}`}>{email}</a>;
 export const patreonUrl = 'https://www.patreon.com/CityTree';
-
-export const meta = {
-  title: 'עץבעיר - מקור ידע לחיים אקולוגיים ולחוסן אקלימי',
-  siteUrl: process.env.GATSBY_SITE_URL,
-  siteName: 'עץבעיר',
-  description: 'מקור ידע לחיים אקולוגיים ולחוסן אקלימי',
-  keywords:
-    "אקולוגיה, אקולוגיה עירונית, ירוק, טבע בעיר, דירה אקולוגית, גינה קהילתית, סביבה ירוקה, קומפוסט, גינון גרילה, סדנת גלידה, שוקולד בריא, גלידה טבעית, מיטפח, קיימות, טיפים אקולוגיים, קהילה אורגנית, חשיבה סביבתית, תזונה מתחשבת, צרכנות אחראית, הפחתה, שימוש מחדש, מיחזור, חקלאות ביתית, גינת מרפסת, טבע בעיר, חיים בני-קיימא, פרמהקלצ'ר, פרמקלצ'ר, אקולוגיה מעשית, מיחזור מים, גידול ירקות, איכות סביבה, בריאות הגוף והסביבה",
-  image: {
-    path: ogImage,
-    width: '765',
-    height: '765',
-  },
-  favicon,
-  author: 'תמי צרי וצוות עץבעיר',
-};
