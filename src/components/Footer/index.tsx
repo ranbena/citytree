ec0d189c36f97a8b6aa697320a2f33c7dbbd043d
Ran Byron <ranbena@gmail.com>
2020-12-21 19:48:07 +0200

    i18n Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,40 +1,49 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Container } from 'react-bootstrap';
-import { MdLink } from 'react-icons/md';
+import { useIntl, FormattedMessage } from 'gatsby-plugin-intl';
+
+import { email } from '@/constants';
+
 import logo from '@/images/logo.png';
-import { phone, email } from '@/constants';
 import FacebookIcon from '@/images/social_facebook.svg';
 import InstagramIcon from '@/images/social_instagram.svg';
 import YoutubeIcon from '@/images/social_youtube.svg';
 import './styles.scss';
 
-const Footer: React.FC = () => (
-  <footer>
-    <Container>
-      <div className="d-flex justify-content-between align-items-center">
-        <a href="http://archives.citytree.net/" target="_blank" rel="noopener noreferrer">
-          <MdLink /> אל האתר הישן והאהוב
-        </a>
-        <img src={logo} alt="CityTree" className="logo" />
-        <div id="info">
-          אנחנו כאן
-          <br />
-          <a href={`tel:${phone}`}>{phone}</a> | <a href={`mailto:${email}`}>{email}</a>
+const Footer: FC = () => {
+  const { formatMessage } = useIntl();
+  const youtube = formatMessage({ id: 'footer.youtube' });
+  const instagram = formatMessage({ id: 'footer.instagram' });
+  const facebook = formatMessage({ id: 'footer.facebook' });
+
+  return (
+    <footer>
+      <Container>
+        <div className="d-flex justify-content-between align-items-center">
+          <div id="info">
+            <FormattedMessage id="footer.text" />
+            <br />
+            <FormattedMessage id="phone">
+              {(phone) => <a href={`tel:${phone}`}>{phone}</a>}
+            </FormattedMessage>{' '}
+            | <a href={`mailto:${email}`}>{email}</a>
+          </div>
+          <img src={logo} alt="CityTree" className="logo" />
           <div className="social">
-            <a href="https://www.youtube.com/user/citytreenet/">
-              <img src={YoutubeIcon} alt="עץבעיר ביוטיוב" />
+            <a href="https://www.youtube.com/user/citytreenet/" title={youtube}>
+              <img src={YoutubeIcon} alt={youtube} />
             </a>
-            <a href="https://www.instagram.com/citytreetlv/">
-              <img src={InstagramIcon} alt="עץבעיר באינסטגרם" />
+            <a href="https://www.instagram.com/citytreetlv/" title={instagram}>
+              <img src={InstagramIcon} alt={instagram} />
             </a>
-            <a href="https://facebook.com/citytree">
-              <img src={FacebookIcon} alt="עץבעיר בפייסבוק" />
+            <a href="https://facebook.com/citytree" title={facebook}>
+              <img src={FacebookIcon} alt={facebook} />
             </a>
           </div>
         </div>
-      </div>
-    </Container>
-  </footer>
-);
+      </Container>
+    </footer>
+  );
+};
 
 export default Footer;
