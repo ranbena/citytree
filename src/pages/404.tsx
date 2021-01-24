ddb3529f59bf1cab277e1d02260583ce89884da5
Ran Byron <ranbena@gmail.com>
2021-01-24 07:20:35 +0200

    Better 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { FC } from 'react';
+import styled from 'styled-components';
 import { PageProps } from 'gatsby';
+import Layout from 'src/components/Layout';
 
-const NotFound: React.FC<PageProps> = () => (
-  <main>
-    <p>Sorry, page not found!</p>
-  </main>
+const Text = styled.h1`
+  text-align: center;
+  margin: 100px 0;
+`;
+
+const NotFound: FC<PageProps> = () => (
+  <Layout>
+    <Text>העמוד לא נמצא</Text>
+  </Layout>
 );
 
 export default NotFound;
