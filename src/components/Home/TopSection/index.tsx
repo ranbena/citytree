63ba3c5b0377d93b4ea59fde00f1828fea59dedb
Ran Byron <ranbena@gmail.com>
2021-03-10 17:32:44 +0200

    Swapped youtube vid as visual

--- src/components/Home/TopSection/index.tsx
+++ src/components/Home/TopSection/index.tsx
@@ -5,9 +5,10 @@ import { useIntl, FormattedMessage } from 'gatsby-plugin-intl';
 import { forEachMessage } from 'src/utils';
 
 import leafImageUrl from 'src/assets/images/leaf-top.png';
-import mainImageUrl from 'src/assets/images/thanks.jpg';
 
 import './styles.scss';
+
+const youtubeId = 'gZpRyenUgKw';
 
 const TopSection: React.FC = () => {
   const intl = useIntl();
@@ -18,23 +19,21 @@ const TopSection: React.FC = () => {
         <Container>
           <Row className="flex-row-reverse">
             <Col lg={7}>
-              {/* {youtubeId && (
               <iframe
                 className="visual"
                 allowFullScreen
                 title="video"
                 width="100%"
                 height="350"
-                src={`https://www.youtube.com/embed/${youtubeId}?origin=http://citytree.com&amp;playsinline=1&amp;color=white&amp;fs=1`}
+                src={`https://www.youtube.com/embed/${youtubeId}?origin=http://citytree.com&amp;playsinline=1&amp;color=white&amp;fs=1&amp;modestbranding=1`}
                 frameBorder="0"
               />
-            )} */}
-              <img
+              {/* <img
                 src={mainImageUrl}
                 className="visual"
                 alt={intl.formatMessage({ id: 'top.visual-text' })}
                 width="100%"
-              />
+              /> */}
             </Col>
             <Col lg={5} className="topSectionText">
               <h1>
