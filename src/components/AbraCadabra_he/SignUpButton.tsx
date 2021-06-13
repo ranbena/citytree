4b1fe33f92e06ee23b8017f302799dddbb5a3cd9
Ran Byron <ranbena@gmail.com>
2021-06-13 19:07:55 +0300

    Added punctuated Hebrew title

--- src/components/AbraCadabra_he/SignUpButton.tsx
+++ src/components/AbraCadabra_he/SignUpButton.tsx
@@ -1,17 +1,17 @@
-import { FormattedMessage } from 'gatsby-plugin-intl';
+import { FormattedMessage, useIntl } from 'gatsby-plugin-intl';
 import React, { FC, PropsWithChildren } from 'react';
 import { Button } from './styles';
 
 const formUrl = 'https://forms.gle/cLW9GkmKsF9szPpt7';
 
 const SignUpButton: FC<PropsWithChildren<{ url?: string | null }>> = ({ url, children }) => {
+  const { formatMessage } = useIntl();
   const onClick = url === null ? undefined : () => window.open(url || formUrl);
+  const title = formatMessage({ id: 'abracadabra-he.name' }).replace(/[\u0591-\u05C7]/g, '');
   return (
     <Button onClick={onClick}>
       {children || <FormattedMessage id="abracadabra-he.signupnow" />}
-      <strong>
-        <FormattedMessage id="abracadabra-he.name" />!
-      </strong>
+      <strong>{title}!</strong>
     </Button>
   );
 };
